55480f1d96d56f73db6bc3e65c96460c884ef998
juliebttran <jbtran@stud.ntnu.no>
2025-02-26 16:43:13 +0100

    test: add unit tests for Die and Dice classes

---
+++ src/test/java/edu/ntnu/DiceTest.java
@@ -0,0 +1,35 @@
+package edu.ntnu;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DiceTest {
+  private Dice dice;
+  @BeforeEach
+  void setUp() {
+    dice = new Dice(2);
+  }
+
+  @Test
+  void testRoll() {
+    int total = dice.roll();
+    assertTrue(total >= 2 && total <= 12);
+  }
+
+  @Test
+  void testGetDieValid() {
+    dice.roll();
+    int value = dice.getDie(1);
+    assertTrue(value >= 1 && value <= 6);
+  }
+
+  @Test
+  void testGetDieInvalid() {
+    IllegalArgumentException exception = assertThrows(
+        IllegalArgumentException.class,
+        () -> dice.getDie(10));
+        assertEquals("Invalid die number.", exception.getMessage());
+  }
+}
