90e0f3ad7fc1573b6c5b4deb28d9d63311118b99
Idaa-i <idaaspenesisaksen@gmail.com>
2025-02-27 14:33:50 +0100

    refactor: Updated Tile with unit test

---
+++ src/test/java/edu/ntnu/board/TileTest.java
@@ -0,0 +1,29 @@
+package edu.ntnu.board;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TileTest {
+    private Tile tile;
+    private TileAction action;
+
+    @BeforeEach
+    void setUp() {
+        tile = new Tile(5);
+        action = position -> position + 2;
+        tile.setAction(action);
+    }
+
+    @Test
+    void testGetPosition() {
+        assertEquals(5, tile.getPosition());
+    }
+
+    @Test
+    void testExecuteAction() {
+        int newPosition = tile.executeAction(5);
+        assertEquals(7, newPosition);
+    }
+}
