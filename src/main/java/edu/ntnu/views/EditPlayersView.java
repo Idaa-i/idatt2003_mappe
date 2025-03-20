69a9c88eacd3ae20ceb3eb105d5550ee05f6b893
Ida <idaaspenesisaksen@gmail.com>
2025-03-20 14:22:51 +0100

    feat: added "createPlayerRow", "removePlayer" and "savePlayersToFile" to now save the players to a textfile

--- src/main/java/edu/ntnu/views/EditPlayersView.java
+++ src/main/java/edu/ntnu/views/EditPlayersView.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 
 public class EditPlayersView extends Application {
     private VBox playersBox;
+    private ArrayList<Player> players = new ArrayList<>();
     private final int MAX_PLAYERS = 4;
     private Label errorLabel;
 
@@ -27,6 +28,7 @@ public class EditPlayersView extends Application {
         addButton.setOnAction(e -> addPlayer());
 
         Button saveButton = new Button("Save players?");
+        saveButton.setOnAction(e -> savePlayersToFile());
 
         Button playButton = new Button("Play!");
 
@@ -50,4 +52,49 @@ public class EditPlayersView extends Application {
         playersBox.getChildren().add(createPlayerRow(newPlayer));
     }
 
+    private HBox createPlayerRow(Player player) {
+        TextField nameField = new TextField(player.getName());
+        nameField.textProperty().addListener((obs, oldVal, newVal) -> player.setName(newVal));
+
+        ComboBox<String> colorPicker = new ComboBox<>();
+        colorPicker.getItems().addAll("Red", "Orange", "Yellow", "Green", "Cyan", "Magenta");
+        colorPicker.setValue("Red");
+        colorPicker.setOnAction(e -> player.setColor(colorPicker.getValue()));
+
+        Button removeButton = new Button("-");
+        removeButton.setOnAction(e -> removePlayer(player, nameField));
+
+        return new HBox(10, nameField, colorPicker, removeButton);
+    }
+
+    private void removePlayer(Player player, TextField nameField) {
+        players.remove(player);
+        playersBox.getChildren().removeIf(node -> ((HBox) node).getChildren().contains(nameField));
+    }
+
+    private void savePlayersToFile() {
+        try (PrintWriter writer = new PrintWriter(new FileWriter("players.txt"))) {
+            for (Player player : players) {
+                writer.println(player.getName() + "," + player.getColor());
+            }
+            errorLabel.setText("Players saved successfully!");
+        } catch (IOException e) {
+            errorLabel.setText("Error saving players!");
+        }
+    }
+
+    static class Player {
+        private String name;
+        private String color;
+
+        public Player(String name) {
+            this.name = name;
+            this.color = "Red"; // Default color
+        }
+
+        public String getName() { return name; }
+        public void setName(String name) { this.name = name; }
+        public String getColor() { return color; }
+        public void setColor(String color) { this.color = color; }
+    }
 }
