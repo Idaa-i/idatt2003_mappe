2e801fd040d296510d2ee4e34a9a6fefb97a50db
juliebttran <jbtran@stud.ntnu.no>
2025-04-09 10:04:48 +0200

    feat: add controller classes

--- src/main/java/edu/ntnu/controller/BoardGameController.java
+++ src/main/java/edu/ntnu/controller/BoardGameController.java
@@ -1,4 +1,51 @@
 package edu.ntnu.controller;
 
+import edu.ntnu.model.BoardGame;
+import edu.ntnu.model.Player;
+import edu.ntnu.model.Tile;
+import edu.ntnu.view.GameView;
+
 public class BoardGameController {
+  private BoardGame model;
+  private GameView view;
+
+  public BoardGameController(BoardGame model, GameView view) {
+    this.model = model;
+    this.view = view;
+    model.addObserver(new BoardGameObserverImpl());
+  }
+
+  public void playTurn() {
+    for (Player player : model.getPlayers()) {
+      if (player.isSkipOneRound()) {
+        view.showMessage(player.getName() + " skips this round");
+        player.setSkipOneRound(false);
+        continue;
+      }
+      int roll = model.rollDice();
+      view.showMessage(player.getName() + " rolled " + roll);
+      player.move(roll, model.getBoard());
+      model.notifyPlayerMove(player);
+      view.updatePlayerPosition(player);
+
+      if (player.hasWon()) {
+        view.showMessage(player.getName() + " wins!");
+        model.setGameOver(true);
+        model.notifyPlayerWon(player);
+        break;
+      }
+    }
+  }
+
+  private class BoardGameObserverImpl implements edu.ntnu.model.BoardGameObserver {
+    @Override
+    public void onPlayerMove(Player player, Tile newPosition) {
+      view.updatePlayerPosition(player);
+    }
+
+    @Override
+    public void onPlayerWin(Player winner) {
+      view.showMessage(winner.getName() + " has won the game!");
+    }
+  }
 }
--- src/main/java/edu/ntnu/controller/LevelController.java
+++ src/main/java/edu/ntnu/controller/LevelController.java
@@ -1,4 +1,26 @@
 package edu.ntnu.controller;
 
+import edu.ntnu.utils.BoardGameFactory;
+import edu.ntnu.model.BoardGame;
+import edu.ntnu.view.SnakesAndLaddersLevelView;
+import edu.ntnu.view.HardSnLGameView;
+import edu.ntnu.view.EasySnLGameView;
+
 public class LevelController {
+  private SnakesAndLaddersLevelView view;
+
+  public LevelController(SnakesAndLaddersLevelView view) {
+    this.view = view;
+  }
+
+  public void openGame(String level) {
+    BoardGame game = BoardGameFactory.createBoardGame(level.toLowerCase(), 2);
+    if (level.equalsIgnoreCase("Hard")) {
+      HardSnLGameView gameView = new HardSnLGameView(game);
+      gameView.start(view.getStage());
+    } else if (level.equalsIgnoreCase("Easy")) {
+      EasySnLGameView gameView = new EasySnLGameView(game);
+      gameView.start(view.getStage());
+    }
+  }
 }
