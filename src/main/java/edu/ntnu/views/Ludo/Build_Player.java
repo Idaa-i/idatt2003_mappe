d5fe57f1aad4072438f7c3f0d94514b8ef343cdf
Ida <idaaspenesisaksen@gmail.com>
2025-05-22 13:04:09 +0200

    docs: Wrote JavaDoc for Build_Player

--- src/main/java/edu/ntnu/views/Ludo/Build_Player.java
+++ src/main/java/edu/ntnu/views/Ludo/Build_Player.java
@@ -1,21 +1,35 @@
 package edu.ntnu.views.Ludo;
-
 import edu.ntnu.model.Player;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * Build_Player class is responsible for visually initializing and managing player pawns
+ * on the Ludo game board. It creates and stores player representation (PlayerWrapper) and handles
+ * their initial graphic placement based on color.
+ * This class acts as a bridge between the Player model and the graphic game interface
+ */
+
 public class Build_Player {
+    /**
+     * Array of player wrappers representing up to four players
+     */
 
     public PlayerWrapper[] players = new PlayerWrapper[4];
+    /**
+     * Initial X positions for pawns of each color
+     */
     int[][] initialX = {
             {1, 1, 3, 3},     // Red
             {10, 10, 12, 12}, // Green
             {10, 10, 12, 12}, // Orange
             {1, 1, 3, 3}      // Purple
     };
+    /**
+     * Initial Y positions for pawns of each color
+     */
     int[][] initialY = {
             {1, 3, 1, 3},
             {1, 3, 1, 3},
@@ -23,6 +37,9 @@ public class Build_Player {
             {10, 12, 10, 12}
     };
 
+    /**
+     * Map from player hex strings to corresponding player index in the array
+     */
     private final HashMap<String, Integer> colorToIndex = new HashMap<>() {{
         put("#E76264", 0); // Red
         put("#719063", 1); // Green
@@ -30,6 +47,13 @@ public class Build_Player {
         put("#9D61E6", 3); // Purple
     }};
 
+    /**
+     * Constructs a Build_Player object that initializes player wrappers from a
+     * list of Player objects
+     * @param logicPlayers List of logical players to be visualized
+     * @param height Height of the game board
+     * @param width Width of the game board
+     */
     public Build_Player(ArrayList<Player> logicPlayers, int height, int width) {
         for (Player p : logicPlayers) {
             String color = p.getColor();
@@ -43,6 +67,10 @@ public class Build_Player {
         }
     }
 
+    /**
+     * Draws all the player tokens onto the GraphicContext
+     * @param gc The graphic context used to draw the players
+     */
     public void draw(GraphicsContext gc) {
         gc.setStroke(Color.BLACK);
         gc.setLineWidth(2);
@@ -53,6 +81,10 @@ public class Build_Player {
         }
     }
 
+    /**
+     * Retrieves the list of logical Player objects from the player wrappers
+     * @return List of logical players represented in this view.
+     */
     public ArrayList<Player> getLogicPlayers() {
         ArrayList<Player> result = new ArrayList<>();
         for (PlayerWrapper wrapper : players) {
