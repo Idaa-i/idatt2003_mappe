8789ee9cf7aaf37a620922d33c02c447654207b2
Ida <idaaspenesisaksen@gmail.com>
2025-05-22 13:29:03 +0200

    docs: Wrote JavaDoc for PlayerWrapper

--- src/main/java/edu/ntnu/views/Ludo/PlayerWrapper.java
+++ src/main/java/edu/ntnu/views/Ludo/PlayerWrapper.java
@@ -1,16 +1,23 @@
 package edu.ntnu.views.Ludo;
-
 import edu.ntnu.model.Player;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 
+/**
+ * The PlayerWrapper class acts as a bridge between the logical representation of a
+ * Player and its visual components in the Ludo game
+ */
 public class PlayerWrapper {
-    private Player logicPlayer;
+    private Player logicPlayer; //The logical player object, holding the player's name and color
     public Pawn[] pawns = new Pawn[4];
     public int coin;
     private int height, width;
 
+    /**
+     * Constructs a PlayerWrapper for the specified logical player, initializing all pawns
+     * @param logicPlayer The logical representation of the player
+     * @param height Height of the boards tiles for rendering
+     * @param width Width of the boards tiles for rendering
+     */
     public PlayerWrapper(Player logicPlayer, int height, int width) {
         this.logicPlayer = logicPlayer;
         this.height = height;
@@ -22,6 +29,13 @@ public class PlayerWrapper {
         }
     }
 
+    /**
+     * Draws the players pawn on the board using the specified GraphicContext
+     * @param gc The graphics context to draw with
+     * @param initialX The initial X positions for each pawn
+     * @param initialY The initial Y positions for each pawn
+     * @param playerIndex The index of the player (used for color and path selection)
+     */
     public void draw(GraphicsContext gc, int[] initialX, int[] initialY, int playerIndex) {
 
         // Draw the 4 pawns
@@ -30,15 +44,26 @@ public class PlayerWrapper {
         }
     }
 
+    /**
+     * Returns the players name
+     * @return The name of the player
+     */
     public String getName() {
         return logicPlayer.getName();
     }
 
+    /**
+     * Returns the players assigned color as hex code string
+     * @return
+     */
     public String getColor() {
         return logicPlayer.getColor();
     }
 
-
+    /**
+     * Returns the underlying logical Player object
+     * @return The logical player
+     */
     public Player getLogicPlayer() {
         return logicPlayer;
     }
