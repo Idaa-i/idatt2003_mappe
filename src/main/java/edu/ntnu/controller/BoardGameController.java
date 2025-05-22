478cc2ebef5b8ffcf2f3554a7cc34d18e79500cc
juliebttran <jbtran@stud.ntnu.no>
2025-05-22 08:51:31 +0200

    docs: add javadoc to all controller classes

--- src/main/java/edu/ntnu/controller/BoardGameController.java
+++ src/main/java/edu/ntnu/controller/BoardGameController.java
@@ -1,12 +1,12 @@
 package edu.ntnu.controller;
 
-import edu.ntnu.model.board.BoardGame;
 import edu.ntnu.model.Player;
+import edu.ntnu.model.board.BoardGame;
 import edu.ntnu.views.BoardGameObserverImplementation;
 import edu.ntnu.views.GameView;
 
 /**
- * Class for handling game logic and mediating the interactions between BoardGame and GameView
+ * Controller class responsible for handling the game logic and mediating interactions.
  */
 public class BoardGameController {
   private BoardGame model;
@@ -14,9 +14,11 @@ public class BoardGameController {
   private int currentPlayerIndex = 0;
 
   /**
-   * Constructs a BoardGameController with the specified model and view.
+   * Constructor for BoardGameController class.
+   * Constructs a BoardGameController with the specified model and view
+   *
    * @param model the BoardGame model to control
-   * @param view the GameView to update and display information
+   * @param view  the GameView to update and display information
    */
   public BoardGameController(BoardGame model, GameView view) {
     this.model = model;
@@ -25,7 +27,9 @@ public class BoardGameController {
   }
 
   /**
-   * Method for executing game turn for all players.
+   * Method for executing a game turn for the current player using the provided dice roll.
+   *
+   * @param roll the result of the dice roll
    */
   public void playTurn(int roll) {
     Player currentPlayer = model.getPlayers().get(currentPlayerIndex);
@@ -50,6 +54,10 @@ public class BoardGameController {
     }
   }
 
+  /**
+   * Method for advancing the turn to the next player in the list.
+   * Wraps around to the first player if the end of the list is reached
+   */
   private void advanceToNextPlayer() {
     currentPlayerIndex = (currentPlayerIndex + 1) % model.getPlayers().size();
   }
